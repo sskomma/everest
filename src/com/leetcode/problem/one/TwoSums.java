eb34baa524f7a1fc76d7005b0912cb7a0ff0bb02
Ram Komma <sskomma@hotmail.com>
2015-10-23 14:40:08 -0500

    adding files

---
+++ src/com/leetcode/problem/one/TwoSums.java
@@ -0,0 +1,58 @@
+package com.leetcode.problem.one;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**Description: 
+ * 
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+ * The function twoSum should return indices of the two numbers such that they add up to the target, 
+ * where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+ * 
+ * You may assume that each input would have exactly one solution.
+ * 
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ * 
+ * @author Ram Komma
+ *
+ */
+public class TwoSums
+{
+
+    public static int[] twoSum(int[] nums, int target) {
+        
+        Map differencesMap = new HashMap();
+        for(int i = 0; i< nums.length; i++)
+        {
+            differencesMap.put(target - nums[i],i);
+        }
+        Integer index = null;
+        int j=0;
+        for(  j=0; j < nums.length; j++)
+        {
+             index = (Integer)differencesMap.get(nums[j]);
+            if(index !=null &&  index!= j)
+            {
+                break; 
+            }
+        }
+        
+        int[] results = {j+1,index+1};
+        return results;
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+
+    }
+
+}
---
+++ src/com/leetcode/problem/three/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+package com.leetcode.problem.three;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+
+    /**
+     * @param args
+     */
+    
+    public int lengthOfLongestSubstring(String s) {
+        
+        return 0;
+    }
+    
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
