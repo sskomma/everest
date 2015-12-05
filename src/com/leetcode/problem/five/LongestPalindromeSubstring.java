d5361c0030a8547e2b434bad230b5f04764ac0c2
Ram Komma <sskomma@hotmail.com>
2015-12-04 18:23:34 -0600

    SubStringSearch problem.
    Solving substring problem with KMP algorithm.

---
+++ src/com/leetcode/problem/five/LongestPalindromeSubstring.java
@@ -0,0 +1,22 @@
+package com.leetcode.problem.five;
+
+/** Given a string S, find the longest palindromic substring in S. 
+ * You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ * 
+ * @author Ram Komma
+ *
+ */
+
+public class LongestPalindromeSubstring
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
