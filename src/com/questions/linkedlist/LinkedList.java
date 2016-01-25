82137faef63ffdfda352da5866bc25ea1dcd27d5
Ram Komma <sskomma@hotmail.com>
2016-01-25 13:42:15 -0600

    Linkedlist problems

--- src/com/questions/linkedlist/LinkedList.java
+++ src/com/questions/linkedlist/LinkedList.java
@@ -1,10 +1,15 @@
 package com.questions.linkedlist;
 
+import java.util.Stack;
+
 public class LinkedList
 {
     private ListNode head;
     
-    /**Adds a node ad the end of the linked list. If the list is empty, it will create a new head. 
+    public LinkedList(){} 
+    public LinkedList(ListNode node){head = node;} 
+    
+    /**Adds a node at the end of the linked list. If the list is empty, it will create a new head. 
      * 
      * @param n number to be placed in the new node. 
      */
@@ -24,8 +29,9 @@ public class LinkedList
     }
 
     /**Removes all nodes that have a given value. 
+     * https://leetcode.com/problems/remove-linked-list-elements/
      * 
-     * @param number
+     * @param number, value of nodes in linked list to be removed. 
      */
     public void removeNodesWithValue(int number)
     {
@@ -60,12 +66,78 @@ public class LinkedList
         return;
     }
     
+    /**Method to delete the nth node from the end of the list. 
+     * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+     * 
+     * @param n, number from the end of list. 
+     */
+    public ListNode removeNthNodeFromEndofList(int n)
+    {
+        ListNode navigator = head;
+        ListNode nthNode = head;
+        ListNode n_1ThNode = null;
+        int i = 1;
+        for(; navigator != null; i++)
+        {
+            navigator = navigator.getNext();
+            if(i > n){
+                n_1ThNode = nthNode;
+                nthNode = nthNode.getNext();
+            }
+                
+        }
+        if(i-1 < n){
+            System.out.println("The linked list does not have n elements in it.");
+            return head;
+        }
+        
+        if(nthNode == head)
+            head = head.next;
+        else
+            n_1ThNode.next = nthNode.next;
+        return head;
+    }
+    
+    /**Returns a reversed linked linked list.
+     * https://leetcode.com/problems/reverse-linked-list/
+     * 
+     * @return
+     */
+    public LinkedList reverseList()
+    {
+        if(head == null)
+            return null;
+        ListNode navigator = head;
+        ListNode reverseHead = null;
+        Stack<ListNode> stack = new Stack<ListNode>();
+        while(navigator!= null)
+        {
+            stack.push(navigator);
+            navigator = navigator.next;
+        }
+        navigator = null;
+        while(!stack.isEmpty())
+        {
+            ListNode popped = stack.pop();
+            if(reverseHead == null){
+                reverseHead = popped;
+                navigator = reverseHead;
+            }
+            else{
+                navigator.next = popped;
+                navigator = navigator.next;
+            }
+            popped.next = null;
+        }
+        return new LinkedList(reverseHead);
+    }
+    
     /**Prints out the linked list
      * 
      */
-    public void printList()
+    public void printList(){printList(head);}
+    public void printList(ListNode node)
     {
-        ListNode node = head;
         if(node != null)
         {
             do
@@ -85,6 +157,12 @@ public class LinkedList
         LinkedList list = new LinkedList();
         list.addNode(1);
         list.addNode(2);
+        list.addNode(3);
+        System.out.println("Linked list before reversing");
+        list.printList();
+        LinkedList n = list.reverseList();
+        System.out.println("Linked list after printing");
+        /*list.addNode(2);
         list.addNode(5);
         list.addNode(5);
         list.addNode(3);
@@ -92,7 +170,8 @@ public class LinkedList
         list.printList();
         list.removeNodesWithValue(5);
         System.out.println("Linked list after printing");
-        list.printList();
+        list.printList();*/
+        
 
     }
 
--- src/com/questions/linkedlist/LinkedListUtils.java
+++ src/com/questions/linkedlist/LinkedListUtils.java
@@ -1,5 +1,7 @@
 package com.questions.linkedlist;
 
+import java.util.Stack;
+
 /**A Utility class with Static methods, to perform various jobs on LinkedList and Nodes.  
  * 
  * @author Ram Komma
@@ -28,29 +30,75 @@ public class LinkedListUtils {
 		
 	}
 	
-	public static void oddEventList(ListNode head)
+	/**Description: A method, to put all nodes in odd positions in list before all even nodes. 
+	 * where, the sequence of odd nodes and even nodes is kept intact. 
+	 * Input   : 1->2->3->4->5->6
+	 * Output  : 1->3->5->2->4->6
+	 * Note: The nodes in odd positions are put upfront and not the elements with odd value.
+	 * https://leetcode.com/problems/odd-even-linked-list/
+	 * 
+	 */
+	public static ListNode oddEventList(ListNode head)
 	{
-		if(head == null || head.next == null)
-			return;
-		ListNode oddListPointer = head;
-		ListNode evenListPointer =head.next;
-		ListNode navigator = head;
-		while(evenListPointer.hasNext())
+		if(head != null && head.next != null)
 		{
-			navigator = evenListPointer.next;
-			ListNode temp = oddListPointer.next;
-			
-			oddListPointer.next = navigator;
+		    //refers to the last odd-node in the list.  
+    		ListNode oddListPointer = head;
+    		//refers to the last even-node in the list.
+    		ListNode evenListPointer =head.next;
+    		//refers to a navigator, that moves in hunt of odd positioned nodes in list. 
+    		ListNode navigator = head;
+    		while(evenListPointer.hasNext())
+    		{
+    			navigator = evenListPointer.next;
+    			ListNode evenListStart = oddListPointer.next;
+    			oddListPointer.next = navigator;
+    			oddListPointer = oddListPointer.next;
+    			ListNode nextEvenNode = navigator.next;
+    			navigator.next = evenListStart;
+    			evenListPointer.next = nextEvenNode;
+    			if(evenListPointer.hasNext())
+    			    evenListPointer = evenListPointer.next;
+    		}
 		}
-		
-		
+		return head;
+	}
+	
+	public static ListNode reverseList(ListNode head)
+	{
+	    if(head == null)
+	        return null;
+	    ListNode navigator = head;
+	    ListNode reverseHead = null;
+	    Stack<ListNode> stack = new Stack<ListNode>();
+	    while(navigator!= null)
+	    {
+	        stack.push(navigator);
+	        navigator = navigator.next;
+	    }
+	    navigator = null;
+	    while(!stack.isEmpty())
+	    {
+	        ListNode popped = stack.pop();
+	        if(reverseHead == null){
+	            reverseHead = popped;
+	            navigator = reverseHead;
+	        }
+	        else
+	            reverseHead.next = popped;
+	        popped.next = null;
+	    }
+	    return reverseHead;
 	}
 	
 	public static void main(String[] args)
 	{
-		ListNode n = new ListNode(0);
-		n.next = new ListNode(1);
-		deleteNode(n);
+	    LinkedList list = new LinkedList();
+	    list.addNode(1);
+	    list.addNode(2);
+	    list.addNode(3);
+	    
+		
 	}
 	
 	
