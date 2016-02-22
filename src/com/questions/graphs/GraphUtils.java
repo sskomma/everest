e89327bede3892acf3664b5fe1880fbfaf8a9c76
Ram Komma <sskomma@hotmail.com>
2016-02-22 10:03:47 -0600

    Graphs and Traversal

---
+++ src/com/questions/graphs/GraphUtils.java
@@ -0,0 +1,115 @@
+package com.questions.graphs;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+public class GraphUtils {
+	
+	/**BFS Algorithm implementation. This method traverses the graph Breadth first 
+	 * and prints each node as it visits it. 
+	 * 
+	 * @param graph
+	 */	
+	public static void BFS(Graph<Integer> graph)
+	{
+		Set<Vertex<Integer>> visited = new HashSet<Vertex<Integer>>();
+		Queue<Vertex<Integer>> vertecesQueue = new LinkedList<Vertex<Integer>>();
+		
+		//Outer for loop is to make sure to print all nodes, even in disconnected graph
+		for(Vertex<Integer> vertex : graph.getAllVerteces())
+		{
+			if(!visited.contains(vertex))
+			{
+				vertecesQueue.add(vertex);
+				while(!vertecesQueue.isEmpty())
+				{
+					Vertex<Integer> v = vertecesQueue.poll();
+					if(!visited.contains(v))
+					{
+						for(Vertex<Integer> adj:v.getAdjcentVerteces())
+						{
+							vertecesQueue.add(adj);
+						}
+						visited.add(v);
+						System.out.println(v);	
+					}
+				}
+			}
+		}
+	}
+
+	/**DFS Algorithm implemented the way BFS is but with a stack to store the next node to visit. 
+	 * This method traverses the graph Depth first and prints each node. 
+	 * 
+	 * @param graph
+	 */
+	public static void DFS1(Graph<Integer> graph)
+	{
+		Set<Vertex<Integer>> visited = new HashSet<Vertex<Integer>>();
+		Stack<Vertex<Integer>> vertecesStack = new Stack<Vertex<Integer>>();
+		for(Vertex<Integer> vertex : graph.getAllVerteces())
+		{
+			if(!visited.contains(vertex))
+			{
+				vertecesStack.add(vertex);
+				while(!vertecesStack.isEmpty())
+				{
+					Vertex<Integer> v = vertecesStack.pop();
+					if(!visited.contains(v)){
+						for(Vertex<Integer> adj:v.getAdjcentVerteces())
+							vertecesStack.add(adj);
+						visited.add(v);
+						System.out.println(v);	
+					}
+				}
+			}
+		}		
+	}
+	
+	/**
+	 * 
+	 * @param graph
+	 */
+	public static void DFS2(Graph<Integer> graph)
+	{
+		Set<Vertex<Integer>> visited = new HashSet<Vertex<Integer>>();
+		for(Vertex<Integer> vertex: graph.getAllVerteces())
+		{
+			if(!visited.contains(vertex))
+				DFS2_VISIT(vertex, visited);
+		}
+			
+	}
+	
+	private static void DFS2_VISIT(Vertex<Integer> vertex, Set<Vertex<Integer>> visited)
+	{
+		visited.add(vertex);
+		System.out.println(vertex);
+		for(Vertex<Integer> adj : vertex.getAdjcentVerteces())
+		{
+			if(!visited.contains(adj))
+				DFS2_VISIT(adj, visited);
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Graph<Integer> graph = new Graph<Integer>(false);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 4);
+		graph.addEdge(1, 3);
+		graph.addEdge(4, 3);
+		graph.addEdge(2, 4);
+		graph.addEdge(4, 6);
+		graph.addEdge(2, 5);
+		graph.addEdge(6, 7);
+		graph.addEdge(5, 7);
+		graph.addEdge(7, 8);
+		DFS2(graph);
+	}
+
+}
--- src/com/questions/graphs/Vertex.java
+++ src/com/questions/graphs/Vertex.java
@@ -1,16 +1,62 @@
 package com.questions.graphs;
 
-public class Vertex {
-	public static enum VertexState {
-		WHITE("Unvisited"), GREY("Discovered"), BLACK("Explored");
-		private VertexState(String state){
-			this.state = state;
-		}
-		public String getState() {
-			return state;
-		}
-		private final String state;
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-	private VertexState state;
+public 	class Vertex<T>{
+	private long id;
+	private T data;
+	private List<Edge<T>> adjcentEdges = new ArrayList<Edge<T>>();
+	private List<Vertex<T>> adjcentVerteces = new ArrayList<Vertex<T>>();
+	
+	public Vertex(long id)
+	{
+		this.id = id;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public List<Vertex<T>> getAdjcentVerteces() {
+		return adjcentVerteces;
+	}
+	public void addAdjcentVertex(Edge<T> edge, Vertex<T> adjcentVertex) {
+		adjcentEdges.add(edge);
+		adjcentVerteces.add(adjcentVertex);
+	}
+	public List<Edge<T>> getAdjcentEdges() {
+		return adjcentEdges;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vertex<T> other = (Vertex<T>) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Vertex [id=" + id + "]";
+	}
 }
